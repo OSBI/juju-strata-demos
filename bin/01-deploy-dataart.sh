ffe9aac747dff855f1db22be2a6be85b43da092d
Samuel Cozannet <samnco@gmail.com>
2015-09-07 17:23:06 +0200

    first release of dataart deployment

--- bin/01-deploy-dataart.sh
+++ bin/01-deploy-dataart.sh
@@ -37,86 +37,32 @@ switchenv "${PROJECT_ID}"
 
 #####################################################################
 #
-# Deploy HBase Environment
-# https://jujucharms.com/u/bigdata-dev/apache-hbase/trusty/14
+# Deploy DeviceHive Environment
 #
 #####################################################################
 
-# Deploy HDFS Master
-deploy apache-hadoop-hdfs-master hdfs-master "mem=4G cpu-cores=2 root-disk=32G"
+# Deploy DeviceHive
+deploy cs:~x3v947pl/trusty/devicehive devicehive "mem=4G cpu-cores=2 root-disk=32G"
 
-# Deploy YARN 
-deploy apache-hadoop-yarn-master yarn-master "mem=2G cpu-cores=2"
+# Deploy PostgreSQL
+deploy cs:~x3v947pl/trusty/postgresql-dh postgresql "mem=2G cpu-cores=2 root-disk=32G"
 
-# Deploy Compute slaves
-deploy apache-hadoop-compute-slave compute-slave "mem=4G cpu-cores=2 root-disk=32G"
-add-unit compute-slave 2
+# Deploy Kafka
+deploy cs:~x3v947pl/trusty/kafka-dh kafka "mem=4G cpu-cores=2 root-disk=32G"
 
-# Deploy Hadoop Plugin
-deploy apache-hadoop-plugin plugin
+# Deploy ZooKeeper
+deploy cs:~x3v947pl/trusty/zookeeper-dh zookeeper "mem=2G cpu-cores=2"
+
+# Deploy nginx
+deploy cs:~x3v947pl/trusty/nginx-dh nginx "mem=2G cpu-cores=2"
 
 # Relations
-add-relation yarn-master hdfs-master
-add-relation compute-slave yarn-master
-add-relation compute-slave hdfs-master
-add-relation plugin yarn-master
-add-relation plugin hdfs-master
-
-#####################################################################
-#
-# Deploy Apache Spark 
-# https://jujucharms.com/apache-spark/trusty/2
-#
-#####################################################################
-
-# Services
-deploy apache-spark spark "mem=4G cpu-cores=2"
-
-# Relations
-add-relation spark plugin
-
-#####################################################################
-#
-# Deploy single node of MongoDB
-# https://jujucharms.com/mongodb/trusty/26
-#
-#####################################################################
-
-#juju deploy --constraints "mem=4G cpu-cores=2 root-disk=16G" mongodb configsvr --config "${MYDIR}/../etc/mongodb-shard.yaml" -n3
-#deploy mongodb mongos
-#juju deploy mongodb shard1 --config "${MYDIR}/../etc/mongodb-shard.yaml" -n3
-#juju deploy mongodb shard2 --config "${MYDIR}/../etc/mongodb-shard.yaml" -n3
-#juju deploy mongodb shard3 --config "${MYDIR}/../etc/mongodb-shard.yaml" -n3
-
-#add-relation mongos:mongos-cfg configsvr:configsvr
-#add-relation mongos:mongos shard1:database
-#add-relation mongos:mongos shard2:database
-#add-relation mongos:mongos shard3:database
-
-deploy mongodb mongodb "mem=4G cpu-cores=2 root-disk=16G"
-add-unit mongodb
-sleep 60
-add-unit mongodb
-
-expose mongodb
-
-#####################################################################
-#
-# Deploy Saiku Server
-# https://jujucharms.com/u/f-tom-n/saikuanalytics/trusty/0 
-#
-#####################################################################
-
-# Tomcat Server
-# https://jujucharms.com/tomcat/trusty/1
-deploy tomcat tomcat "mem=8G cpu-cores=4"
-
-# Saiku
-deploy cs:~f-tom-n/trusty/saikuanalytics-0 saiku
-
-# Relations
-add-relation tomcat saiku
+add-relation zookeeper:ka kafka:zk
+add-relation zookeeper:dh devicehice:zk
+add-relation kafka:dh devicehive:ka
+add-relation devicehive:pg postgresql:dh
+add-relation nginx:dh devicehive:website
 
 # Expose
-expose tomcat
+expose nginx
 
