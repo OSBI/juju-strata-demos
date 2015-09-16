3a1238119bc69ff9a68ffb7141a12f6a23b3fad1
Tom Barber <tom@analytical-labs.com>
2015-09-16 14:24:27 +0100

    mongo stuff

--- bin/02-deploy-saiku-demo.sh
+++ bin/02-deploy-saiku-demo.sh
@@ -61,3 +61,13 @@ action do saiku/0 adddatasource content="type=OLAP\nname=taxi\ndriver=mondrian.o
 
 action do saiku/0 addreport content="$(cat ../demo_files/demo_1.saiku)" path="/homes/home:admin/demo_1.saiku"
 #action do saiku/0 addschema name=mongo content="$(cat ./mongo_schema.xml)"
+
+run --unit mongodb/0 "apt-get update && apt-get install unzip && wget http://84.40.61.82/trip_fare_1.csv.zip && unzip trip_fare_1.csv.zip"
+
+run --unit mongodb/0 --timeout 10m0s "mongoimport -d taxi -c fares --type csv --file trip_fare_1.csv --headerline"
+
+action do saiku/0 addschema name=mongo content="$(cat ../demo_files/mongo_schema.xml)"
+
+action do saiku/0 addmongoschema content="$(cat ../demo_files/mongo_db)"
+
+action do saiku/0 adddatasource content="type=OLAP\nname=taxi-mongo\ndriver=mondrian.olap4j.MondrianOlap4jDriver\nlocation=jdbc:mondrian:Jdbc=jdbc:calcite:model=mongo:///etc/mongoschema/taxi;Catalog=mondrian:///datasources/mongo.xml;JdbcDrivers=net.hydromatic.optiq.jdbc.Driver;\nusername=admin\npassword=admin"
