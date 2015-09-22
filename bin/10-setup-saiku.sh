46f744dbf5155b74641fd7a5301b93cb47803bcc
Samuel Cozannet <samnco@gmail.com>
2015-09-22 16:42:38 +0200

    new versions

--- bin/10-setup-saiku.sh
+++ bin/10-setup-saiku.sh
@@ -87,4 +87,20 @@ juju action do saiku/0 addschema name=mongo content="$(cat ${MYDIR}/../var/mongo
 
 juju action do saiku/0 addmongoschema content="$(sed -e s/MONGODB_HOST/${MONGODB_PRIVATE_IPADDRESS}/g ${MYDIR}/../var/mongo_db)"
 
-juju action do saiku/0 adddatasource content="type=OLAP\nname=taxi-mongo\ndriver=mondrian.olap4j.MondrianOlap4jDriver\nlocation=jdbc:mondrian:Jdbc=jdbc:calcite:model=mongo:///etc/mongoschema/taxi;Catalog=mondrian:///datasources/mongo.xml;JdbcDrivers=net.hydromatic.optiq.jdbc.Driver;\nusername=admin\npassword=admin"
+juju action do saiku/0 adddatasource content="type=OLAP\nname=taxi-mongo\ndriver=mondrian.olap4j.MondrianOlap4jDriver\nlocation=jdbc:mondrian:Jdbc=jdbc:calcite:model=mongo:///etc/mongoschema/taxi;Catalog=mondrian:///datasources/mongo.xml;JdbcDrivers=org.apache.calcite.jdbc.Driver;\nusername=admin\npassword=admin"
+
+juju action do saiku/0 addreport content=$(cat ${MYDIR}/../var/avg_fare_amount_by_day.saiku) path="/homes/home:admin/avg_fare_amount_by_day.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_p_count_by_day_mdx.saiku)" path="/homes/home:admin/avg_p_count_by_day_mdx.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_p_count_by_day_rotate.saiku)" path="/homes/home:admin/avg_p_count_by_day_rotate.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_p_count_by_day.saiku)" path="/homes/home:admin/avg_p_count_by_day.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_p_count_by_hour_rotate.saiku)" path="/homes/home:admin/avg_p_count_by_hour_rotate.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_p_count_by_hour.saiku)" path="/homes/home:admin/avg_p_count_by_hour.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_t_dist_by_day_rotate.saiku)" path="/homes/home:admin/avg_t_dist_by_day_rotate.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_t_dist_by_day.saiku)" path="/homes/home:admin/avg_t_dist_by_day.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_t_dist_by_hour.saiku)" path="/homes/home:admin/avg_t_dist_by_hour.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_tip_amount_by_day.saiku)" path="/homes/home:admin/avg_tip_amount_by_day.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_total_amount_by_day.saiku)" path="/homes/home:admin/avg_total_amount_by_day.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_trip_time_by_day_rotate.saiku)" path="/homes/home:admin/avg_trip_time_by_day_rotate.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_trip_time_by_day.saiku)" path="/homes/home:admin/avg_trip_time_by_day.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/avg_trip_time_by_hour.saiku)" path="/homes/home:admin/avg_trip_time_by_hour.saiku"
+juju action do saiku/0 addreport content="$(cat ${MYDIR}/../var/max_tips_waterfall.saiku)" path="/homes/home:admin/max_tips_waterfall.saiku"
--- var/mongodb-taxi.sh
+++ var/mongodb-taxi.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Make sure we have tools
-apt-get update -yqq && apt-get install -yqq unzip 
+#################################################################################
+#
+# This is a big fat hack to upgrade MongoDB to 2.6 so it works.
+#
+#
+#################################################################################
+service mongodb stop
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-get update -yqq
+apt-get -yqq install mongodb-org unzip
+sed -i 's/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g' /etc/mongod.conf
+service mongod restart
 
 # Download taxi file if not there
 cd /home/ubuntu
@@ -11,6 +22,14 @@ cd /home/ubuntu
 
 # Import data into MongoDB
 mongoimport -d taxi -c fares --type csv --file trip_fare_1.csv --headerline
+
+mongo taxi --eval "db.fares.ensureIndex({hack_license:1})"
+mongo taxi --eval "db.fares.ensureIndex({medallion:1})"
+mongo taxi --eval "db.fares.ensureIndex({vendor_id:1})"
+mongo taxi --eval "db.fares.ensureIndex({payment_type:1})"
+mongo taxi --eval "db.fares.ensureIndex({pickup_datetime:1})"
+mongo taxi --eval "db.fares.ensureIndex({day:1})"
+mongo taxi --eval "db.fares.ensureIndex({hour:1})"
 
 rm -f trip_fare_1.csv.zip trip_fare_1.csv
 
