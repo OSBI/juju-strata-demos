7e3d0632b2b09cd744bd7a053f12baf59c03c709
Tom Barber <tom@analytical-labs.com>
2015-09-24 15:12:24 +0100

    limit to 10000

--- var/spark-store.sh
+++ var/spark-store.sh
@@ -12,7 +12,7 @@ su -c "hdfs dfs -put /home/ubuntu/store.par /user/ubuntu" ubuntu
 
 /usr/lib/spark/bin/beeline -u jdbc:hive2://localhost:10000 -e 'CREATE TABLE STORE(medallion varchar(250),hack_license varchar(250),vendor_id varchar(250),rate_code int,store_and_fwd_flag varchar(250),pickup_datetime timestamp,dropoff_datetime timestamp,passenger_count int,trip_time_in_secs int,trip_distance double,pickup_longitude double,pickup_latiude double,dropoff_longitude double,dropoff_latitude double,t_id bigint,year int,month int,day int,hour int)'
 
-/usr/lib/spark/bin/beeline -u jdbc:hive2://localhost:10000 -e 'insert into table store select * from store2'
+/usr/lib/spark/bin/beeline -u jdbc:hive2://localhost:10000 -e 'insert into table store select * from store2 limit 10000'
 
 rm -f store.tgz
 rm -rf store.par
